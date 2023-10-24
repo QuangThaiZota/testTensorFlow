852efef8b4d1784e5705574fa223f334db6f235d
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-10-24 10:48:24 +0700

    using model yolo v8

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -4,6 +4,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_vision/flutter_vision.dart';
 
 class ScanController extends GetxController{
 
@@ -25,9 +26,12 @@ class ScanController extends GetxController{
   late List<CameraDescription> cameras;
   late CameraImage cameraImage;
   var x,y,w,h = 0.0;
+  var x1,x2,y1,y2 =0.0;
   var label="ádfjdkj";
   var cameraCout = 0;
+  var imgHeight, imgWidth = 0;
   var isCameraInitialized = false.obs;
+  FlutterVision vision = FlutterVision();
 
 
   initCamera() async{
@@ -60,13 +64,22 @@ class ScanController extends GetxController{
   }
 
   initTFlite() async{
-    await Tflite.loadModel(
-      model: "assets/model1.tflite",
-      labels: "assets/label1.txt",
-      isAsset: true,
-      numThreads: 1,
-      useGpuDelegate: false,
-    );
+    // await Tflite.loadModel(
+    //   model: "assets/model1.tflite",
+    //   labels: "assets/label1.txt",
+    //   isAsset: true,
+    //   numThreads: 1,
+    //   useGpuDelegate: false,
+    // );
+
+    //Use Yolo v8
+    await vision.loadYoloModel(
+        modelPath: 'assets/yolov8n.tflite',
+        labels: 'assets/yololabels.txt',
+        modelVersion: "yolov8",
+        quantization: false,
+        numThreads: 1,
+        useGpu: false);
   }
 
   objectDectector(CameraImage image) async{
@@ -85,19 +98,19 @@ class ScanController extends GetxController{
     // );
 
     ///Using SSDMobileNet
-    var detector = await Tflite.detectObjectOnFrame(
-        bytesList: image.planes.map((plane) {return plane.bytes;}).toList(),// required
-        model: "SSDMobileNet",
-        // model: "YOLO",
-        imageHeight: image.height,
-        imageWidth: image.width,
-        imageMean: 127.5,   // defaults to 127.5
-        imageStd: 127.5,    // defaults to 127.5
-        rotation: 90,       // defaults to 90, Android only
-        // numResults: 2,      // defaults to 5
-        threshold: 0.1,     // defaults to 0.1
-        asynch: true        // defaults to true
-    );
+    // var detector = await Tflite.detectObjectOnFrame(
+    //     bytesList: image.planes.map((plane) {return plane.bytes;}).toList(),// required
+    //     model: "SSDMobileNet",
+    //     // model: "YOLO",
+    //     imageHeight: image.height,
+    //     imageWidth: image.width,
+    //     imageMean: 127.5,   // defaults to 127.5
+    //     imageStd: 127.5,    // defaults to 127.5
+    //     rotation: 90,       // defaults to 90, Android only
+    //     // numResults: 2,      // defaults to 5
+    //     threshold: 0.1,     // defaults to 0.1
+    //     asynch: true        // defaults to true
+    // );
 
     // var detector = await Tflite.detectObjectOnFrame(
     //     bytesList: image.planes.map((plane) {return plane.bytes;}).toList(),// required
@@ -115,25 +128,82 @@ class ScanController extends GetxController{
     //     asynch: true          // defaults to true
     // );
 
+    var detector = await vision.yoloOnFrame(
+        bytesList: image.planes.map((plane) => plane.bytes).toList(),
+        imageHeight: image.height,
+        imageWidth: image.width,
+        iouThreshold: 0.4,
+        confThreshold: 0.4,
+        classThreshold: 0.5);
 
 
-    if(detector != null){
+
+    // if(detector != null){
+    //   log("Result is $detector ");
+    //   var ourDectectedObject = detector.first;
+    //     if(ourDectectedObject['confidenceInClass'] * 100 > 60) {
+    //       log("Result is $ourDectectedObject ");
+    //       label = ourDectectedObject['detectedClass'];
+    //       print("height $label");
+    //       h = ourDectectedObject["rect"]["h"];
+    //       print("height $h");
+    //       w = ourDectectedObject["rect"]["w"];
+    //       print("width $w");
+    //       y = ourDectectedObject["rect"]["y"];
+    //       x = ourDectectedObject["rect"]["x"];
+    //     }
+    //   update();
+    //   // }
+    // }
+
+
+    //Bắn data với yolov8
+    imgHeight = image.height;
+    imgWidth = image.width;
+    if (detector != null) {
       log("Result is $detector ");
-      var ourDectectedObject = detector.first;
-        if(ourDectectedObject['confidenceInClass'] * 100 > 60) {
-          log("Result is $ourDectectedObject ");
-          label = ourDectectedObject['detectedClass'];
-          print("height $label");
-          h = ourDectectedObject["rect"]["h"];
-          print("height $h");
-          w = ourDectectedObject["rect"]["w"];
-          print("width $w");
-          y = ourDectectedObject["rect"]["y"];
-          x = ourDectectedObject["rect"]["x"];
+      var ourDetectedObject = detector.first;
+      var box = ourDetectedObject['box'];
+      if (box[4] * 100 >=50) {
+        log("Result is $ourDetectedObject ");
+        label = ourDetectedObject['tag'];
+        print("Detected Class: $label");
+
+        if (ourDetectedObject['box'] != null) {
+
+          print("Result Box: $box");
+          print("ScanController:");
+          // x1 = double.parse(box[0]);
+          // print("ScanController x1: $x1");
+          // y1 = double.parse(box[1]);
+          // print("ScanController y1: $y1");
+          // x2 = double.parse(box[2]);
+          // print("ScanController x2: $x2");
+          // y2 = double.parse(box[3]);
+          // print("ScanController 5: $y2");
+          // var classConfidence = double.parse(box[4]);
+          // print("Bounding Box Coordinates 6:");
+          // print("x1: $x1, y1: $y1, x2: $x2, y2: $y2");
+
+          x1 = double.tryParse(box[0].toString()) ?? box[0];
+          print("ScanController x1: $x1");
+          y1 = double.tryParse(box[1].toString()) ?? box[1];
+          print("ScanController y1: $y1");
+          x2 = double.tryParse(box[2].toString()) ?? box[2];
+          print("ScanController x2: $x2");
+          y2 = double.tryParse(box[3].toString()) ?? box[3];
+          print("ScanController 5: $y2");
+          var classConfidence = double.tryParse(box[4].toString()) ?? box[4];
+          print("Bounding Box Coordinates 6:");
+          print("x1: $x1, y1: $y1, x2: $x2, y2: $y2");
+          print("Class Confidence: $classConfidence");
+          // var classConfidence = box['class_confidence'];
+          print("Class Confidence: $classConfidence");
         }
+      }
       update();
-      // }
     }
+
 
   }
 }
--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -12,22 +12,43 @@ class CameraView extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
 
+
     return Scaffold(
       body: GetBuilder<ScanController>(
         init: ScanController(),
         builder: (controller) {
-          print("Controller weight: ${controller.w}");
-          print("Controller label: ${controller.label}");
+          double factorX = screenWidth / (controller.imgWidth);
+          double imgRatio = screenWidth / controller.imgHeight;
+          double newWidth = screenWidth * factorX;
+          double newHeight = newWidth / imgRatio;
+          double factorY = newHeight / (screenHeight);
+
+          double pady = (screenHeight - newHeight) / 2;
+          print("CameraView x1: ${controller.x1}");
+          print("CameraView x2: ${controller.x2}");
+          print("CameraView y1: ${controller.y1}");
+          print("CameraView y2: ${controller.y2}");
+          print("CameraView label: ${controller.label}");
+          print("CameraView imgHeight: ${controller.imgHeight}");
+          print("CameraView imgWidth: ${controller.imgWidth}");
           return controller.isCameraInitialized.value
               ? Stack(
                 children: [
                   CameraPreview(controller.cameraController),
                   Positioned(
-                    top: (controller.y)*(screenHeight),
-                    left: (controller.x)*(screenWidth),
+                    // top: (controller.y)*(screenHeight),
+                    // left: (controller.x)*(screenWidth),
+                    left: controller.x1 * factorX,
+                    top: controller.y1 * factorY+ pady,
+                    width: (controller.x2 - controller.x1) * factorX,
+                    height: (controller.y2 - controller.y1) * factorY,
+                    // left: 100,
+                    // top: 100,
+                    // width: 100,
+                    // height: 100,
                     child: Container(
-                      width:  (controller.w)*context.width,
-                      height: (controller.h)*context.height,
+                      // width:  (controller.w)*context.width,
+                      // height: (controller.h)*context.height,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(8),
                         border: Border.all(color: Colors.green,width: 4),
